830a4ed0063371422d64e93e7ea30642de936534
Artandor <millt.nico@hotmail.fr>
2021-09-19 01:31:59 +0200

    Close Add privacy policy and some links #9

---
+++ src/Controller/PrivacyPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: "/privacy-policy", name: "privacy")]
+final class PrivacyPolicy extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render('privacy.html.twig');
+    }
+}
--- src/Form/StreamInfoType.php
+++ src/Form/StreamInfoType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\TitleHistory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,9 @@ class StreamInfoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('category')
-            ->add('submit', SubmitType::class);
+            ->add('title', TextType::class, ['label' => 'stream.title'])
+            ->add('category', TextType::class, ['label' => 'stream.category'])
+            ->add('submit', SubmitType::class);//TODO Translate that
     }
 
     public function configureOptions(OptionsResolver $resolver)
